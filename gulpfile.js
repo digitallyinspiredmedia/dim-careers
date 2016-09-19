c82001210aec90ae316639051f906a3a83c3abe5
S.I.Kavinraj <sikavinraj@gmail.com>
2016-09-19 22:10:24 +0530

    verions 0.2

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'), // init
       plumber = require('gulp-plumber'), // plumber - error log
       concat = require('gulp-concat'), // concat - connect all file into one
       sourcemaps = require('gulp-sourcemaps'), // sourcemaps generater
-      autoprefixer = require('gulp-autoprefixer'), // add pre-fix for all browser [  look on versions  and  implementation ]
+      autoprefixer = require('gulp-autoprefixer'), // add pre-fix for all browser
+      imagemin = require('gulp-imagemin'),
       browserSync = require('browser-sync'),
       critical = require('critical');
 
@@ -17,11 +18,11 @@ gulp.task('browser-sync', function() {
     });
 });
 gulp.task('uglify', function(){
-  gulp.src('js/*.js')
+  gulp.src('js/other.js')
   .pipe(sourcemaps.init())
   .pipe(plumber())
   .pipe(uglify())
-  .pipe(concat('all.js'))
+  .pipe(concat('others.js'))
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('src/js/'))
   .pipe(browserSync.reload({stream: true})); // prompts a reload after compilation
@@ -46,12 +47,18 @@ gulp.task('css',function(){
     console.log('Uglify CSS fucked from MANUAL ');
 });
 
+gulp.task('image', () =>
+    gulp.src('image/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('src/image'))
+);
+
+
 gulp.task('watch', ['browser-sync', 'css', 'uglify'], function(){
    console.log('Browser Sync fucked off MANUAL RELOAD');
   gulp.watch("css/*.css", ['css']);
-  gulp.watch("js/*.js" , ['uglify']);
+  //gulp.watch("js/*.js" , ['uglify']);
   gulp.watch("*.html").on('change', browserSync.reload);
-  // Other watchers
 });
 
 gulp.task('critical', function (cb) {
@@ -59,10 +66,11 @@ gulp.task('critical', function (cb) {
         inline: true,
         base: '.',
         src: 'index.html',
-        dest: 'src/index.php',
+        dest: 'src/index.html',
         minify: true,
         width: 320,
-        height: 480
+        height: 480,
+        timeout: 900000
     });
 });
 
